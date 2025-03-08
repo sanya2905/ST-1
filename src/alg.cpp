0926dcd02da82911c4df4eb516638b7395cb1c6d
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-03-08 19:40:57 +0300

    com 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,44 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+	if (value < 2) {
+		return false;
+	}
+	for (int i = 2; i * i <= value; i++) {
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+	int c = 0;
+	int number = 1;
+	while (c < n) {
+		number++;
+		if (checkPrime(number)) {
+			c++;
+		}
+	}
+	return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+
+	uint64_t next = value + 1;
+	while (!(checkPrime(next))) {
+		next++;
+	}
+	return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	uint64_t sum = 0;
+	for (int i = 2; i < hbound; i++) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
+	}
+	return sum;
 }
