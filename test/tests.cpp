7889b1328dcbcb8fab97ae61a9eef3beac9bf956
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-03-08 22:32:14 +0300

    com 6 fix build

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,6 +8,7 @@
 TEST(st1, CheckPrime1) {
     EXPECT_FALSE(checkPrime(5000));
 }
+
 TEST(st1, CheckPrime2) {
     EXPECT_TRUE(checkPrime(157));
 }
@@ -15,6 +16,7 @@ TEST(st1, CheckPrime2) {
 TEST(st1, CheckPrime3) {
     EXPECT_FALSE(checkPrime(75));
 }
+
 TEST(st1, CheckPrime4) {
     EXPECT_TRUE(checkPrime(113));
 }
@@ -23,14 +25,9 @@ TEST(st1, SumPrime1) {
     uint64_t res = sumPrime(2000000);
     uint64_t expected = 142913828922;
     EXPECT_EQ(expected, res);
-    uint64_t res = sumPrime(2000000);
-    uint64_t expected = 142913828922;
-    EXPECT_EQ(expected, res);
 }
+
 TEST(st1, SumPrime2) {
-    uint64_t res = sumPrime(10);
-    uint64_t expected = 17;
-    EXPECT_EQ(expected, res);
     uint64_t res = sumPrime(10);
     uint64_t expected = 17;
     EXPECT_EQ(expected, res);
@@ -47,6 +44,7 @@ TEST(st1, nPrime1) {
     uint64_t expected = 17;
     EXPECT_EQ(expected, res);
 }
+
 TEST(st1, nPrime2) {
     uint64_t res = nPrime(40);
     uint64_t expected = 173;
@@ -64,6 +62,7 @@ TEST(st1, nextPrime1) {
     uint64_t expected = 109;
     EXPECT_EQ(expected, res);
 }
+
 TEST(st1, nextPrime2) {
     uint64_t res = nextPrime(5);
     uint64_t expected = 7;
